7c34f6153299e1b4664784c6f95ba457132e27a4
wenchong <18256786+wenchonglee@users.noreply.github.com>
2021-12-06 00:22:15 +0800

    Added public url

--- utils/fetchMetadata.ts
+++ utils/fetchMetadata.ts
@@ -2,7 +2,7 @@ import { UrlMetaData } from "../pages/api/scrape";
 
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://your_deployment.server.com";
+export const server = dev ? "http://localhost:3000" : "https://rich-link-preview.vercel.app";
 export const fetchMetadata = async (url: string): Promise<UrlMetaData | null> => {
   try {
     const response = await fetch(`${server}/api/scrape?url=${url}`);
